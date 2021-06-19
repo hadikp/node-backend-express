5558617ecdfe6729bec830eab14695e58c9308b1
hadikp <hp20001025@gmail.com>
2021-06-19 20:45:44 +0200

    mesterremek list

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dashboard' });
 });
 
+router.get('/ingredients', function(req, res, next) {
+  res.render('ingredients', { title: 'Étel összetevők listája' });
+});
+
 module.exports = router;
